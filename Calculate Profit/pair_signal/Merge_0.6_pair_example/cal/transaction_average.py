591425203632fe49b2cd67eed7f5999411126b37
박진석 <68769481+jjpark51@users.noreply.github.com>
2022-12-28 18:34:59 +0900

    Update transaction_average.py

--- Calculate Profit/pair_signal/Merge_0.6_pair_example/cal/transaction_average.py
+++ Calculate Profit/pair_signal/Merge_0.6_pair_example/cal/transaction_average.py
@@ -26,7 +26,7 @@ csv_files = []
 
 ticker = []    
 
-filename = open("[PATH_OF_TRANSACTION_CSV_FILE]", "r") # ex) YOLO-Profit-Calculation/Calculate Profit/pair_signal/Merge_0.6_five_example/cal/2019 Transaction Result
+filename = open("[PATH_OF_TRANSACTION_CSV_FILE]", "r") # ex) YOLO-Profit-Calculation/Calculate Profit/pair_signal/Merge_0.6_five_example/cal/Transaction Result.csv
 
 csv_file = csv.DictReader(filename)
 ticker = []
